14be4a6713f0185f3fd299b0c53d3dd514906ed4
ArielM24 <ariel_montoya15@outlook.es>
2020-03-05 12:34:40 -0600

    sorted tf*idf

--- sort_nouns.py
+++ sort_nouns.py
@@ -1,4 +1,30 @@
 import operator
+import math
+
+def get_freq(tups):
+	nouns_freq = {}
+	for n in tups:
+		if n[0] not in nouns_freq:
+			nouns_freq[n[0]] = 1
+		nouns_freq[n[0]] = nouns_freq[n[0]] + 1
+	return nouns_freq
+
+def get_ifdt(dict_freq,k):
+	tokens = dict_freq.keys()
+	n = len(tokens)
+
+	tf_idf = {}
+	for token in tokens:
+		x = dict_freq[token]
+		tf_idf[token] = (((k + 1)*x)/(x+k))*math.log(n/x,2)
+
+	return tf_idf
+
+def print_file_tups(file, tups):
+	f = open(file,"w")
+	for t in tups:
+		f.write(t[0]+" "+str(t[1])+"\n")
+	f.close()
 
 if __name__ == '__main__':
 	fnouns = open("lemma_nouns.txt","r")
@@ -8,15 +34,11 @@ if __name__ == '__main__':
 		nouns.append((l[0],l[1]))
 	fnouns.close()
 
-	nouns_freq = {}
-	for n in nouns:
-		if n[0] not in nouns_freq:
-			nouns_freq[n[0]] = 1
-		nouns_freq[n[0]] = nouns_freq[n[0]] + 1
-
+	nouns_freq = get_freq(nouns)
 	sorted_nouns = sorted(nouns_freq.items(), key=operator.itemgetter(1), reverse=True)
+	print_file_tups("sorted_freq_nouns.txt",sorted_nouns)
 	
-	fsorted = open("sorted_nouns.txt","w")
-	for n in sorted_nouns:
-		fsorted.write(n[0]+" "+str(n[1])+"\n")
-	fsorted.close()
+
+	tf_idf = get_ifdt(nouns_freq,1.2)
+	sorted_tf = sorted(tf_idf.items(),key=operator.itemgetter(1), reverse=True)
+	print_file_tups("sorted_ifd.txt",sorted_tf)
