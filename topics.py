1985275eb3cda4fd570d6dedda7a19de5be72214
ArielM24 <ariel_montoya15@outlook.es>
2020-03-10 09:39:27 -0600

    tabla

--- topics.py
+++ topics.py
@@ -1,4 +1,5 @@
 import re
+from prettytable import PrettyTable
 from bs4 import BeautifulSoup
 
 def get_articles(file):
@@ -24,19 +25,17 @@ def mine_topics(topics, articles):
 			count = a.count(topic)
 			count = (count/sz)*100
 			cw.append(count)
-		freq[a[0] + str(num)] = cw
+		freq[a[0] + " " +str(num)] = cw
 		num = num + 1
 	return freq
 
 if __name__ == '__main__':
-	articles = get_articles("e961024.htm")
-	topics = ["crisis","privatización","contaminación","politica","economía","tecnología","televisa"]
-	freq = mine_topics(topics, articles)
-
-	arts = freq.keys()
-	print("\t",end="")
-	for t in topics:
-		print(t,end="\t")
-	print()
-	for a in arts:
-		print(a,freq[a])
+    articles = get_articles("e961024.htm")
+    topics = ["crisis","privatización","contaminación","politica","economía","tecnología","televisa"]
+    freq = mine_topics(topics, articles)
+    arts = freq.keys()
+    t = ["id_articulo","crisis","privatización","contaminación","politica","economía","tecnología","televisa"]
+    table= PrettyTable(t)
+    for i in arts:
+        table.add_row([i,freq[i][0],freq[i][1],freq[i][2],freq[i][3],freq[i][4],freq[i][5],freq[i][6]])
+    print(table)
